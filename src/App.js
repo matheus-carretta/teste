ca878161ac6501618f308a75a51c3797aa45c003
matheus-carretta <matheuscarretta@gmail.com>
2022-07-11 13:52:44 -0300

    f

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save togitreload.
+          Edit <code>src/App.js</code> and save togit   reload.
         </p>
         <a
           className="App-link"
